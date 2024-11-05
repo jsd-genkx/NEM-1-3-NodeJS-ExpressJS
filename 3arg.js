2ddc49aa22f1d675a42887efca11bc52e7ac914b
Khem <tempkhem@gmail.com>
2024-11-05 18:50:32 +0700

    fix(example&exercise3)

--- 3arg.js
+++ 3arg.js
@@ -3,10 +3,13 @@
 const argv = process.argv;
 console.log(process.argv);
 
-if (argv[2]) {
-  console.log("This is arg3 = ", argv[2]);
+if (argv[2] === 'a' || argv[2] === 'aaa') {
+  console.log("This is arg2 = ", argv[2]);
 }
 
-if (argv[3]) {
-  console.log("This is arg4 = ", argv[3]);
+if (!isNaN(argv[3])) {
+  console.log("This is arg3 = ", argv[3]);
+  for (let i = 0; i < 10; i++) {
+    console.log(i);
+  }
 }
